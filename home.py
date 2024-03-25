a7cafce23cb160e0dbe46345362e15fed0903c46
Tulasiram21 <86564556+Tulasiram21@users.noreply.github.com>
2024-03-25 06:08:34 +0530

    Update home.py

--- home.py
+++ home.py
@@ -7,30 +7,6 @@ import numpy as np
 import subprocess
 
 def app():
-    class_names = ['Aloevera', 'Amla', 'Amruta_Balli', 'Arali', 'Ashoka', 'Ashwagandha', 'Avacado', 'Bamboo', 'Basale', 'Betel', 'Betel_Nut',
-              'Brahmi', 'Castor', 'Curry_Leaf', 'Doddapatre', 'Ekka', 'Ganike', 'Gauva', 'Geranium', 'Henna', 'Hibiscus', 'Honge', 'Insulin', 'Jasmine',
-              'Lemon', 'Lemon_grass', 'Mango', 'Mint', 'Nagadali', 'Neem', 'Nithyapushpa', 'Nooni', 'Pappaya', 'Pepper', 'Pomegranate', 'Raktachandini', 'Rose',
-              'Sapota', 'Tulasi', 'Wood_sorel']
-    def preprocess_image(image):
-        image = image.resize((128, 128))
-    # Convert the image to numpy array
-        img_array = np.array(image)
-    # Normalize the pixel values
-        img_array = img_array / 255.0
-        return img_array
-
-    def predict(loaded_model, image):
-    # Preprocess the image
-        img_array = preprocess_image(image)
-        img_array = np.expand_dims(img_array, axis=0)
-
-    # Make predictions
-        predictions = loaded_model.predict(img_array)
-        predicted_class = class_names[np.argmax(predictions[0])]
-        confidence = round(100 * np.max(predictions[0]), 2)
-        return predicted_class, confidence
-
-# Main Streamlit app
     st.subheader("Welcome to Ayurvedic Plant Detection via Image processing")
     st.title(' Image Detection  WebApp')
 
